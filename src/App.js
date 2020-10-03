103bf1f6b68aded69616aebfccd3b1cc04e09fda
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-10-02 21:59:55 -0400

    created components and set up router in main.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-
 import './App.css';
-
+import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './Components/Main';
+import {Link} from 'react-router-dom';
 class App extends Component {
   render(){
     return(
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+<div style={{height: '300px', position: 'relative'}}>
+    <Layout fixedHeader>
+        <Header title={<span><span style={{ color: '#ddd' }}></span><strong>Nargiza Boronchieva</strong></span>}>
+            <Navigation>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
     )
   }
 }
